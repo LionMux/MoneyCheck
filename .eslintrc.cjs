eb0db3c56b4affcf52f285d7282ad8ac26945ad5
Лев Мухачев <163592011+LionMux@users.noreply.github.com>
2026-04-08 12:03:15 +0300

    fix(eslint): add parserOptions sourceType:module and ecmaFeatures:jsx

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,11 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
   rules: {
     'no-unused-vars': 'off',
     'no-undef': 'off',
